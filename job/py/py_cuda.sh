f160848193f646dfb2b71a39b05a588259dc3d00
o70664d <o70664d@ito-1.kyushu-u.sc>
2020-04-28 15:27:37 +0900

    Add shell script for single python with cuda

---
+++ job/py/py_cuda.sh
@@ -0,0 +1,37 @@
+to
+#-----------------------------------------------------
+# sub-system
+#PJM -L "rscunit=ito-b"
+
+# resource group
+#PJM -L "rscgrp=ito-g-4-dbg"
+
+# number of nodes
+#PJM -L "vnode=1"
+
+# number of cores per node
+#PJM -L "vnode-core=36"
+
+# elapsed-time limit
+#PJM -L "elapse=1:00"
+
+# Output standard error to the same file that standard output
+#PJM -j
+#-----------------------------------------------------
+
+# cuda vesion
+#-----------------------------------------------------
+module load cuda/10.1
+#-----------------------------------------------------
+
+
+# python version
+#-----------------------------------------------------
+module load python/3.6.2
+#-----------------------------------------------------
+
+
+# run
+#-----------------------------------------------------
+python3 main.py
+#-----------------------------------------------------
